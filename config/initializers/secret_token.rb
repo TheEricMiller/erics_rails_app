b4aae40ee16443256139ec208248592b0fe43d1d
Eric Miller <Eric@TheEricMiller.com>
2013-03-14 03:26:10 -0400

    created new Ruby on Rails app >$ rails new erics_rails_app >$ cd erics_rails_app >$ bundle update >$ git init
    + updated .gitignore + README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EricsRailsApp::Application.config.secret_token = 'cea6123955e951c4f14bc7f57ea967691f6e6c70bf7d765d53ffaf7684307cc3510f4230540ae0e769d487967eca792c4652c51c057d91b5e3aff8ab8d9c7eaa'

13ec9d03aa13d238221e7f6ca4c8d1194bee0ea9
Eric Miller <Eric@TheEricMiller.com>
2013-03-15 20:58:21 -0400

    configured devise 'user' model + added fields to migration for profile_name, time_zone, time_zone_active + added modules confirmable, lockable >$ rake db:migrate
    > Add User (see readme)
    > http://localhost:3000/users/sign_in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+  		:profile_name, :time_zone, :time_zone_active
+
+  		
   # attr_accessible :title, :body
 end
